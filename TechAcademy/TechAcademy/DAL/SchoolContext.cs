cb9005a2b908f44fc9aaffa8f839d878ac8b0672
Michael Tallant <tall3655@gmail.com>
2019-04-04 18:06:50 -0600

    Create TechAcademy final exercise

---
+++ TechAcademy/TechAcademy/DAL/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using TechAcademy.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TechAcademy.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
